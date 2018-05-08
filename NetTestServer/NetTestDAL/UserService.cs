2e6d5159e11328d49fe542525c6f77c214f9101f
wx98 <wx.98@qq.com>
2018-05-08 16:33:46 +0800

    整理本工程的文件但并未实现
    具体错误不明

---
+++ NetTestServer/NetTestDAL/UserService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetTestModel;
+using System.Data;
+
+namespace NetTestDAL
+{
+    public class UserService
+    {
+        public string login(UserClass user)
+        {
+            string s = "failed";
+            using (SqlConnection con = new SqlConnection(DBHelper.conString))
+            {
+                DBHelper DB = new DBHelper(con);
+                SqlParameter pName = new SqlParameter("@uName", SqlDbType.Char);
+                pName.Value = user.uName;
+                SqlParameter pPass = new SqlParameter("@uPass", SqlDbType.Char);
+                pPass.Value = user.uPass;
+                try
+                {
+                    DB.executeCommand("insert into users values(@uName,@uPass)", pName, pPass);
+                    s = "registered";
+
+                }
+                catch
+                {
+                    if (DB.getScalar("select count(*) from users where uName=@uName and uPass=@uPass", pName, pPass) > 0)
+                        s = "logined";
+                    else
+                        s = "failed";
+
+                }
+                con.Close();
+            }
+            return s;
+
+        }
+    }
+
+}
---
+++ NetTestServer/NetTest_Database.sql
@@ -0,0 +1,7 @@
+create database NetTest_Database;
+use NetTest_Database;
+create table users
+(
+	uName varchar (32) primary key,
+	uPass varchar (512)
+)
