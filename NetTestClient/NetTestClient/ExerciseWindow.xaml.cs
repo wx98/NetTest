f59ba03489c5677deb429ec8dbdc7738b663b336
wx98 <wx.98@qq.com>
2018-05-15 00:12:19 +0800

    完成试题练习功能的代码编写（未找Bug）
    （未找Bug）

--- NetTestClient/NetTestClient/ExerciseWindow.xaml.cs
+++ NetTestClient/NetTestClient/ExerciseWindow.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Security.Cryptography;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -17,24 +22,130 @@ namespace NetTestClient
     /// <summary>
     /// ExerciseWindow.xaml 的交互逻辑
     /// </summary>
+    /// 
+    public class AnswerClass : List<String>
+    {
+        public AnswerClass()
+        {
+            Add("A"); Add("B"); Add("C"); Add("D");
+        }
+    }
     public partial class ExerciseWindow : Window
     {
-        
+        WCF.NetTestServiceClient client = new WCF.NetTestServiceClient();
+        string url = "http://localhost:8889/NetTestService/";
+        DataTable dt;
+        DataView dv;
         public ExerciseWindow()
         {
             InitializeComponent();
+            
+        }
+        void client_getUserTestDataTableCompleted(object sender, WCF.getUserTestDataTableCompletedEventArgs e)
+        {
+            if (e.Error == null)
+            {
+                dt = e.Result;
+                dt.Columns.Add("tNo");
+                dt.Columns.Add("uAnswer");
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    dt.Rows[i]["tNo"] = (i + 1).ToString();
+                    dt.Rows[i]["uAnswer"] = "";
+                }
+                dt.AcceptChanges();
+                dv = dt.DefaultView;
+                uGrid.ItemsSource = dv;
+                btGetTest.IsEnabled = false;
+                btHandleTest.IsEnabled = true;
+            }
+            else showMsg(e.Error.Message);
+        }
+    
+        void showMsg(String s)
+        {
+            MessageBox.Show(s, "Information", MessageBoxButton.OK);
+        }
+        String encryptString(String s)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] buf = Encoding.UTF8.GetBytes(s);
+            buf = md5.ComputeHash(buf);
+            s = "";
+            foreach (byte x in buf)
+                s = s + x.ToString("X2");
+            return s;
         }
 
+        private void uGrid_selectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (uGrid.SelectedIndex >= 0)
+            {
+                txtText.DataContext = dv[uGrid.SelectedIndex];
+            }
+            else
+            {
+                txtText.DataContext = null;
+            }
+        }
+        private void btHandleTest_Click(object sender, RoutedEventArgs e)
+        {
+            int mValue = 0;
+            foreach (DataRow row in dt.Rows)
+            {
+                if (row["tAnswer"].ToString() == row["uAnswer"].ToString())
+                    ++mValue;
+            }
+            msg.Text = "成绩:" + mValue.ToString();
+            btHandleTest.IsEnabled = true;
+            String uName =  Get_Info.User;
+            String uPass = Get_Info.Pass;
+            if (uName != "" && uPass != "")
+            {
+                try
+                {
+                    uPass = encryptString(uPass);
+                    using (OperationContextScope scope = new OperationContextScope(client.InnerChannel))
+                    {
+                        MessageHeader user = MessageHeader.CreateHeader("uName", "MySpace", uName);
+                        MessageHeader pass = MessageHeader.CreateHeader("uPass", "MySpace", uPass);
+                        OperationContext.Current.OutgoingMessageHeaders.Add(user);
+                        OperationContext.Current.OutgoingMessageHeaders.Add(pass);
+                        client.setUserMarkAsync(mValue);
+                    }
+                }
+                catch (Exception exp)
+                {
+                    showMsg(exp.Message);
+                }
+            }
+            
+        }
         
 
         private void btGetTest_Click(object sender, RoutedEventArgs e)
         {
-
-        }
-
-        private void btHandleTest_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
+            String uName = Get_Info.User;
+            String uPass = Get_Info.Pass;
+            if (uName != "" && uPass != "")
+            {
+                try
+                {
+                    uPass = encryptString(uPass);
+                    using (OperationContextScope scope = new OperationContextScope(client.InnerChannel))
+                    {
+                        MessageHeader user = MessageHeader.CreateHeader("uName", "MySpace", uName);
+                        MessageHeader pass = MessageHeader.CreateHeader("uPass", "MySpace", uPass);
+                        OperationContext.Current.OutgoingMessageHeaders.Add(user);
+                        OperationContext.Current.OutgoingMessageHeaders.Add(pass);
+                        client.getTestDataTableAsync();
+                    }
+                }
+                catch (Exception exp)
+                {
+                    showMsg(exp.Message);
+                }
+            }
+        } 
     }
 }
